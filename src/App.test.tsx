5aaea67996894f702a10b169a6dcb0f026314670
Alexandre Joris <alex@blockchainpartner.fr>
2022-06-19 15:14:39 +0200

    :wrench: fixing tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from './routes/Login';
 import IncidentBrowser from './routes/IncidentsBrowser';
+import { BrowserRouter } from 'react-router-dom';
 
 
 test('renders the login page', () => {
-  render(<Login />);
+  render(
+    <BrowserRouter>
+      <Login />
+    </BrowserRouter>
+  );
   const login = screen.getAllByText('Login')[0];
   expect(login).toBeInTheDocument();
 });
 
 
 test('renders the IncidentBrowser page', () => {
-  render(<IncidentBrowser />);
+  const [open, setOpen] = React.useState(false);
+
+  render(
+  <BrowserRouter>
+    <IncidentBrowser  menuOpen={open} setMenuOpen={setOpen}/>
+  </BrowserRouter>);
   const incidentBrowser = screen.getAllByText('Incidents')[0];
   expect(incidentBrowser).toBeInTheDocument();
 });
