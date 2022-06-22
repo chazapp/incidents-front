4ca283673c806c67e12f77ede6b54f0c2184b049
Alexandre Joris <alex@blockchainpartner.fr>
2022-06-23 00:11:51 +0200

    :sparkles: added Save/Delete buttons

--- src/components/IncidentCard.tsx
+++ src/components/IncidentCard.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Box, Paper, Divider, FormControl, InputLabel, InputAdornment, IconButton, OutlinedInput, Select, MenuItem } from "@mui/material";
+import { Box, Paper, Divider, FormControl, InputLabel, InputAdornment, IconButton, OutlinedInput, Select, MenuItem, TextareaAutosize, Typography } from "@mui/material";
 import { Incident } from "../index.d";
 import EditIcon from '@mui/icons-material/Edit';
-
+import SendIcon from "@mui/icons-material/Send";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
 
 function IncidentCard(props: { incident: Incident }) {
     const { incident } = props;
@@ -11,14 +13,20 @@ function IncidentCard(props: { incident: Incident }) {
     const [ status, setStatus ] = React.useState("");
     const [ description, setDescription ] = React.useState("");
 
+    const [ editTitle, setEditTitle ] = React.useState(false);
+    const [ editDescription, setEditDescription ] = React.useState(false);
+
     React.useEffect(() => {
         setTitle(incident.title);
         setSeverity(incident.severity);
         setStatus(incident.status);
         setDescription(incident.description);
+        if (incident.id === -1) {
+            setEditDescription(true);
+            setEditTitle(true);
+        }
     }, [incident]);
 
-    const [ editTitle, setEditTitle ] = React.useState(false);
     return (
         <Paper sx={{
             display: "flex",
@@ -54,39 +62,47 @@ function IncidentCard(props: { incident: Incident }) {
                         }
                     />
                 </FormControl>
-
                 <Box aria-label="incident-infos" sx={{
                     display: "flex",
                     flexDirection: "row",
-                    gap: "1rem",
                 }}>
-                    <FormControl sx={{minWidth: 120 }}>
-                        <InputLabel id="incident-severity">Severity</InputLabel>
-                        <Select
-                            labelId="incident-severity"
-                            label="Severity"
-                            value={severity}
-                            onChange={(e) => setSeverity(e.target.value)}
-                            data-cy="incident-severity-select"
-                        >
-                            <MenuItem value="Low">Low</MenuItem>
-                            <MenuItem value="Medium">Medium</MenuItem>
-                            <MenuItem value="High">High</MenuItem>
-                        </Select>
-                    </FormControl>
-                    <FormControl  sx={{minWidth: 120 }}>
-                        <InputLabel id="incident-status">Status</InputLabel>
-                        <Select
-                            labelId="incident-status"
-                            label="Severity"
-                            value={status}
-                            onChange={(e) => setStatus(e.target.value)}
-                            data-cy="incident-status-select"
-                        >
-                            <MenuItem value="Open">Open</MenuItem>
-                            <MenuItem value="Closed">Closed</MenuItem>
-                        </Select>
-                    </FormControl>
+                    <Box sx={{ display: "flex", flexGrow: "1", gap: "1rem"}}>
+                        <FormControl sx={{minWidth: 120 }}>
+                            <InputLabel id="incident-severity">Severity</InputLabel>
+                            <Select
+                                labelId="incident-severity"
+                                label="Severity"
+                                value={severity}
+                                onChange={(e) => setSeverity(e.target.value)}
+                                data-cy="incident-severity-select"
+                            >
+                                <MenuItem value="Low">Low</MenuItem>
+                                <MenuItem value="Medium">Medium</MenuItem>
+                                <MenuItem value="High">High</MenuItem>
+                            </Select>
+                        </FormControl>
+                        <FormControl  sx={{minWidth: 120 }}>
+                            <InputLabel id="incident-status">Status</InputLabel>
+                            <Select
+                                labelId="incident-status"
+                                label="Severity"
+                                value={status}
+                                onChange={(e) => setStatus(e.target.value)}
+                                data-cy="incident-status-select"
+                            >
+                                <MenuItem value="Open">Open</MenuItem>
+                                <MenuItem value="Closed">Closed</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box sx={{ display: "flex", gap: "1rem" }}>
+                        <Button variant="outlined" color="error" startIcon={<DeleteIcon />}>
+                            Delete
+                        </Button>
+                        <Button variant="contained" endIcon={<SendIcon />}>
+                            Save
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
             <Divider />
@@ -97,8 +113,19 @@ function IncidentCard(props: { incident: Incident }) {
                 padding: "1rem",
                 whiteSpace: "pre-wrap",
                 wordBreak: "break-all",
-            }}>
-                {description}
+                }}
+                onClick={() => {
+                    setEditDescription(true);
+                }}
+                >
+                {editDescription ? 
+                <TextareaAutosize style={{height: "100%"}} value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    data-cy="incident-description-input"
+                    onBlur={() => { setEditDescription(false); }}
+                />
+                : <Typography>{description}</Typography>
+                }
             </Box>
         </Paper>
     );
