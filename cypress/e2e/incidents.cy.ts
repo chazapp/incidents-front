f55fde9d1407ca615c868d9c2b999886035fc5ef
Alexandre Joris <alex@blockchainpartner.fr>
2022-07-06 00:38:47 +0200

    :green_check_mark: added CRUD tests

--- cypress/e2e/incidents.cy.ts
+++ cypress/e2e/incidents.cy.ts
@@ -1,12 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Incidents CRUD', () => {
+
+    // Recursively find an element in table
+    function findIncidentInTable(incidentName: string) {
+        cy.get("[data-cy='incident-table']").then((item) => {
+            if (item.text().includes(incidentName)) {
+                return;
+            } else {
+                cy.get("[aria-label='incident-table-next-page']").then((item) => {
+                    if (!item.prop('disabled')) {
+                        cy.get("[aria-label='incident-table-next-page']").click();
+                        findIncidentInTable(incidentName);
+                    } else {
+                        throw new Error(`Could not find ${incidentName} in table`);
+                    }
+                })
+            }
+        })
+    }
+
     it('should be able to create, update and delete an incident', () => {
             cy.session("CRUD Incident", () => {
                 const email = Cypress.env("INCIDENTS_USER");
             const password = Cypress.env("INCIDENTS_PASSWORD");
             
             cy.login(email, password);
+            // Create an incident
             cy.get("[data-cy=incident-add]").click();
             cy.get("[data-cy=incident-title-input]").type("Test Incident");
             cy.get("[data-cy=incident-severity-select]").click();
@@ -16,6 +36,15 @@ describe('Incidents CRUD', () => {
             cy.get("[data-cy=incident-description]").click()
             cy.get("[data-cy=incident-description-input]").type("Test Incident Description");
             cy.get("[data-cy=incident-submit]").click();
+            // Assert the incident was created and is present in the table
+            findIncidentInTable("Test Incident");
+            // Update the incident
+            cy.get("[data-cy=incident-table]").contains("Test Incident").click();
+            cy.get("[data-cy=incident-edit-title]").click();
+            cy.get("[data-cy=incident-title-input]").clear().type("Test Incident Updated");
+            cy.get("[data-cy=incident-severity-select]").click();
+            cy.get("[data-cy=incident-severity-medium]").click();
+
         });
     });
   });
