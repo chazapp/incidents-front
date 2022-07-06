d28fee98bd7694db719352ccfa09b7a1fde5173d
Alexandre Joris <alex@blockchainpartner.fr>
2022-07-06 17:29:52 +0200

    :wrench: added render tests on stateless components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from './routes/Login';
 import IncidentBrowser from './routes/IncidentsBrowser';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
 import App from './App';
+import IncidentCard from './components/IncidentCard';
+import { Incident } from '.';
+import IncidentSearch from './components/IncidentSearch';
+import PersistentDrawer from './components/Navigation';
 
 
 test('renders the login page', () => {
@@ -15,3 +19,33 @@ test('renders the login page', () => {
   const login = screen.getAllByText('Login')[0];
   expect(login).toBeInTheDocument();
 });
+
+test('renders the incident card component', () => {
+  const incident: Incident = {
+    id: 1,
+    title: 'Test Incident',
+    description: "",
+    status: 'Open',
+    severity: 'Low',
+    created_at: '2020-01-01',
+    updated_at: '2020-01-01',
+  };
+
+  render(
+    <IncidentCard 
+      incident={incident}
+      onCreate={() => {}}
+      onUpdate={() => {}}
+      onDelete={() => {}}
+    />
+  );
+});
+
+test('renders the incident search component', () => {
+  render(
+    <IncidentSearch
+      setIncidents={() => {}}
+      setIsLoading={() => {}}
+    />
+  )
+})
