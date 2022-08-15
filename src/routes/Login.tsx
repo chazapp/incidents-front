190dee86ae7e002094e574c92572580b28862443
Alexandre Joris <alex@blockchainpartner.fr>
2022-08-15 23:49:18 +0200

    :fire: removed white back ground css rules for darktheme

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -108,7 +108,6 @@ function Login() {
                 display: "flex",
                 width: "25vw",
                 height: "100vh",
-                backgroundColor: "white",
             }}>
                 <LoginForm />
             </Box>
